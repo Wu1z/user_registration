0c61f83bead35a9c5f2b93fdfb753d44258d4da6
Wu1z <wllmerscher@gmail.com>
2022-05-17 18:08:51 -0300

    Criando tela de lista de usuários

--- lib/user_list/view/user_list_page.dart
+++ lib/user_list/view/user_list_page.dart
@@ -12,9 +12,35 @@ class _UserListPageState extends State<UserListPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Users"),
+        title: const Text("Users"),
       ),
-      body: Container(),
+      body: ListView.separated(
+        itemCount: 10,
+        physics: const BouncingScrollPhysics(),
+        separatorBuilder: (context, index) => const Divider(thickness: 1),
+        itemBuilder: (_, index) {
+          return ListTile(
+            contentPadding: const EdgeInsets.all(8),
+            leading: Icon(
+              Icons.person, 
+              color: Theme.of(context).colorScheme.primary,
+              size: 30,
+            ),
+            title: Text(
+              "Nome $index",
+              style: const TextStyle(fontSize: 16),
+            ),
+            trailing: const Icon(
+              Icons.arrow_forward_rounded,
+            ),
+            onTap: () => _goToProfilePage(),
+          );
+        }
+      ),
     );   
+  }
+
+  _goToProfilePage() {
+    debugPrint('');
   }
 }
