4dcffbff4ee577e6f081a2209cb3e358c50e1134
Wu1z <wllmerscher@gmail.com>
2022-05-16 22:23:51 -0300

    Implementando tela de login e de lista de usuários

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:user_registration/theme/theme.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:user_registration/login/view/login_page.dart';
+import 'package:user_registration/shared/theme/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,56 +16,13 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        useMaterial3: true,
         colorScheme: lightColorScheme,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        textTheme: GoogleFonts.montserratTextTheme(
+          Theme.of(context).textTheme,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: const LoginPage(),
     );
   }
 }
---
+++ lib/user_list/view/user_list_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class UserListPage extends StatefulWidget {
+  const UserListPage({Key? key}) : super(key: key);
+
+  @override
+  State<UserListPage> createState() => _UserListPageState();
+}
+
+class _UserListPageState extends State<UserListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Users"),
+      ),
+      body: Container(),
+    );   
+  }
+}
