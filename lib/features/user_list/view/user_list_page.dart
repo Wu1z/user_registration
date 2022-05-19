6957e43e4876f950b794f624eb3902243f639432
Wu1z <wllmerscher@gmail.com>
2022-05-19 10:35:55 -0300

    Implentando campo de pesquisa na tela lista de pessoas

--- lib/features/user_list/view/user_list_page.dart
+++ lib/features/user_list/view/user_list_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:user_registration/features/user_profile/view/user_profile_page.dart';
+import 'package:user_registration/shared/widgets/default_text_field.dart';
 
 class UserListPage extends StatefulWidget {
   const UserListPage({Key? key}) : super(key: key);
@@ -9,6 +11,9 @@ class UserListPage extends StatefulWidget {
 }
 
 class _UserListPageState extends State<UserListPage> {
+
+  final _searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,29 +24,51 @@ class _UserListPageState extends State<UserListPage> {
         child: const Icon(Icons.add),
         onPressed: () => _goToProfilePage(),
       ),
-      body: ListView.separated(
-        itemCount: 10,
-        physics: const BouncingScrollPhysics(),
-        separatorBuilder: (context, index) => const Divider(height: 1),
-        itemBuilder: (_, index) {
-          return ListTile(
-            contentPadding: const EdgeInsets.all(8),
-            leading: Icon(
-              Icons.person, 
-              color: Theme.of(context).colorScheme.primary,
-              size: 30,
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: DefaultTextField(
+              label: "Search", 
+              controller: _searchController,
+              inputType: TextInputType.number,
+              inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly
+              ],
+              icon: const Icon(Icons.search),
+              suffixIcon: IconButton(
+                icon: const Icon(Icons.clear),
+                onPressed: () => _searchController.clear(),
+              ),
             ),
-            title: Text(
-              "Nome $index",
-              style: const TextStyle(fontSize: 16),
+          ),
+          Expanded(
+            child: ListView.separated(
+              itemCount: 10,
+              physics: const BouncingScrollPhysics(),
+              separatorBuilder: (context, index) => const Divider(height: 1),
+              itemBuilder: (_, index) {
+                return ListTile(
+                  contentPadding: const EdgeInsets.all(8),
+                  leading: Icon(
+                    Icons.person, 
+                    color: Theme.of(context).colorScheme.primary,
+                    size: 30,
+                  ),
+                  title: Text(
+                    "Nome $index",
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  trailing: Icon(
+                    Icons.arrow_forward_ios_rounded,
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+                  onTap: () => _goToProfilePage(),
+                );
+              }
             ),
-            trailing: Icon(
-              Icons.arrow_forward_ios_rounded,
-              color: Theme.of(context).colorScheme.secondary,
-            ),
-            onTap: () => _goToProfilePage(),
-          );
-        }
+          ),
+        ],
       ),
     );   
   }
